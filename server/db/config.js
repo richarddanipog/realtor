be77a6cb7b8f3715e6300550679497505f7ad00f
RichardDanipog <55653353+richarddanipog@users.noreply.github.com>
2020-02-10 16:58:25 +0200

    update config

--- server/db/config.js
+++ server/db/config.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql');
 
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Rr9550268!',
+//     database: 'realtor'
+// });
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Rr9550268!',
-    database: 'realtor'
+    host: 'l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'cyskir9b10ru5sto',
+    password: 'nenb3le88kgl6ner',
+    database: 'b4hgq84ack2bsl4k'
 });
 
 module.exports = connection;
